9fec13bcd802c4b67abace8a84b423d09b809dc7
monoqlo <monoqlo44@gmail.com>
2012-10-05 17:57:44 +0900

    ModalViewController追加。

---
+++ UnwindSeguesTest/ViewController/MoalViewController.h
@@ -0,0 +1,14 @@
+//
+//  MoalViewController.h
+//  UnwindSeguesTest
+//
+//  Created by 米山 隆貴 on 2012/10/05.
+//  Copyright (c) 2012年 米山 隆貴. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MoalViewController : UIViewController
+
+- (IBAction)dissmissModalView:(id)sender;
+@end
---
+++ UnwindSeguesTest/ViewController/MoalViewController.m
@@ -0,0 +1,35 @@
+//
+//  MoalViewController.m
+//  UnwindSeguesTest
+//
+//  Created by 米山 隆貴 on 2012/10/05.
+//  Copyright (c) 2012年 米山 隆貴. All rights reserved.
+//
+
+#import "MoalViewController.h"
+
+@interface MoalViewController ()
+
+@end
+
+@implementation MoalViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)dissmissModalView:(id)sender {
+    [[self presentingViewController] dismissViewControllerAnimated:YES completion:nil];
+    
+    // selfに飛ばしてやってもpresentingViewCOntrollerにメッセージを送ってくれるらしい
+    //[self dismissViewControllerAnimated:YES completion:nil];
+}
+@end
